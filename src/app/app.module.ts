77f726a4baa5f540b0c8071ace6d0a638b9608a1
takumi <t.endo@fujinetworks.co.jp>
2020-11-16 14:07:42 +0900

    最初のプッシュ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { CustomerListComponent } from './maintenance/customer-list/customer-list.component';
+import { EmployeeListComponent } from './maintenance/employee-list/employee-list.component';
+import { RouterModule} from '@angular/router';
+import { MainMenuComponent } from './common/main-menu/main-menu.component';
+import { OrderEntryComponent } from './maintenance/order-entry/order-entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainMenuComponent,
+    CustomerListComponent,
+    EmployeeListComponent,
+    OrderEntryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: MainMenuComponent },
+      { path: 'customer', component: CustomerListComponent },
+      { path: 'employee', component: EmployeeListComponent },
+      { path: 'order', component: OrderEntryComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/maintenance/order-entry/order-entry.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-entry',
+  templateUrl: './order-entry.component.html',
+  styleUrls: ['./order-entry.component.scss']
+})
+export class OrderEntryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
